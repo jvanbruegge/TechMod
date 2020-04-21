80859bfeed015bac90fdf36acf83833a3ff46215
Jan van Brügge <supermanitu@gmail.com>
2020-04-21 20:05:30 +0200

    Fix the model rotation points

--- src/main/java/com/jvanbruegge/techmod/cablecar/CablecarModel.java
+++ src/main/java/com/jvanbruegge/techmod/cablecar/CablecarModel.java
@@ -1,46 +1,44 @@
-package com.jvanbruegge.techmod.cablecar;//Made with Blockbench
-//Paste this code into your mod.
+package com.jvanbruegge.techmod.cablecar;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.vertex.IVertexBuilder;
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.client.renderer.model.ModelRenderer;
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.api.distmarker.OnlyIn;
 
 public class CablecarModel extends EntityModel<CablecarEntity> {
-	private final ModelRenderer hook;
-	private final ModelRenderer body;
+    private final ModelRenderer hook;
+    private final ModelRenderer body;
 
-	public CablecarModel() {
-		textureWidth = 16;
-		textureHeight = 16;
+    public CablecarModel() {
+        textureWidth = 16;
+        textureHeight = 16;
 
-		hook = new ModelRenderer(this);
-		hook.setRotationPoint(0F, 0F, 0F);
-		hook.addBox(-5F, 5.75F, -1F, 1F, 8F, 2F, 0F);
-		hook.addBox(-4F, 12.75F, -1F, 8F, 1F, 2F, 0F);
-		hook.addBox(-2.5F, 13.75F, -0.5F, 0.5F, 1F, 1F, 0F);
-		hook.addBox(2F, 13.75F, -0.5F, 0.5F, 1F, 1F, 0F);
-		hook.addBox(1F, 14.25F, -0.5F, 1F, 0.5F, 1F, 0F);
-		hook.addBox(-2F, 14.25F, -0.5F, 1F, 0.5F, 1F, 0F);
-		hook.addBox(4F, 5.75F, -1F, 1F, 8F, 2F, 0F);
+        hook = new ModelRenderer(this);
+        hook.setRotationPoint(0F, 16F, 0F);
+        hook.addBox(-5F, -10.25F, -1F, 1F, 8F, 2F, 0F);
+        hook.addBox(-4F, -3.25F, -1F, 8F, 1F, 2F, 0F);
+        hook.addBox(-2.5F, -2.25F, -0.5F, 0.5F, 1F, 1F, 0F);
+        hook.addBox(2F, -2.25F, -0.5F, 0.5F, 1F, 1F, 0F);
+        hook.addBox(1F, -1.75F, -0.5F, 1F, 0.5F, 1F, 0F);
+        hook.addBox(-2F, -1.75F, -0.5F, 1F, 0.5F, 1F, 0F);
+        hook.addBox(4F, -10.25F, -1F, 1F, 8F, 2F, 0F);
 
-		body = new ModelRenderer(this);
-		body.setRotationPoint(0F, -8F, 0F);
-		hook.addChild(body);
-		body.addBox(-4F, 2.75F, -5F, 8F, 1F, 10F, 0F);
-		body.addBox(-4F, 3.75F, -5F, 1F, 4F, 10F, 0F);
-		body.addBox(3F, 3.75F, -5F, 1F, 4F, 10F, 0F);
-		body.addBox(-3F, 3.75F, 4F, 6F, 4F, 1F, 0F);
-		body.addBox(-3F, 3.75F, -5F, 6F, 4F, 1F, 0F);
-	}
-
-    @Override
-    public void setRotationAngles(CablecarEntity entity, float f1, float f2, float f3, float f4, float f5) {}
+        body = new ModelRenderer(this);
+        body.setRotationPoint(0F, -9.25F, 0F);
+        hook.addChild(body);
+        body.addBox(-4F, -4F, -5F, 8F, 1F, 10F, 0F);
+        body.addBox(-4F, -3F, -5F, 1F, 4F, 10F, 0F);
+        body.addBox(3F, -3F, -5F, 1F, 4F, 10F, 0F);
+        body.addBox(-3F, -3F, 4F, 6F, 4F, 1F, 0F);
+        body.addBox(-3F, -3F, -5F, 6F, 4F, 1F, 0F);
+    }
 
     @Override
-    public void render(MatrixStack stack, IVertexBuilder builder, int i1, int i2, float f1, float f2, float f3, float f4) {
-        hook.render(stack, builder, i1, i2, f1, f2, f3, f4);
+    public void setRotationAngles(CablecarEntity entityIn, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch) {
+    }
+
+    @Override
+    public void render(MatrixStack matrix, IVertexBuilder builder, int packedLightIn, int packedOverlayIn, float red, float green, float blue, float alpha) {
+        hook.render(matrix, builder, packedLightIn, packedOverlayIn, red, green, blue, alpha);
     }
 }
